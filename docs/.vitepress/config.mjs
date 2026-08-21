3cce6502959f133f1e05067fb336fb7eb2453ab3
LiarOnce <liaronce@hotmail.com>
2026-08-21 16:50:50 +0800

    feat: add search bar
    Signed-off-by: LiarOnce <liaronce@hotmail.com>

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,40 +1,43 @@
-import { defineConfig } from "vitepress";
-import { resolve } from "path";
-import { fileURLToPath } from "url";
+import { defineConfig } from 'vitepress'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
 
-import { zh } from "./locales/zh";
+import { zh } from './locales/zh'
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        "@": resolve(__dirname, ".."),
-      },
-    },
-  },
-  description: "AOSC Mascots website",
-  themeConfig: {
-    socialLinks: [
-      {
-        icon: {
-          svg: '<svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg"><g transform="translate(0,-50)"><circle cx="501.58" cy="552.36" r="475.19" fill="#000"/><circle cx="524" cy="550.36" r="54.07" fill="#fff"/><path d="M513.84 176.91A373.65 373.65 0 0 0 140.19 550.57 373.65 373.65 0 0 0 513.84 924.22v-182.5a191.87 191.87 0 0 1-181.71-191.36 191.87 191.87 0 0 1 181.71-191.51z" fill="#c00000"/><path d="M514.77 394.05a156.09 156.09 0 0 0-156.09 156.09 156.09 156.09 0 0 0 156.09 156.08v-74.27a82.15 82.15 0 0 1-72.92-81.59 82.15 82.15 0 0 1 72.92-81.53z" fill="#0070c0"/><path d="M531.57 230.53v160.87a159.34 159.34 0 0 1 151.77 158.96 159.34 159.34 0 0 1-151.77 159.07v160.87a319.89 319.89 0 0 0 319.88-319.9 319.89 319.89 0 0 0-319.88-319.87z" fill="#ffc000"/></g></svg>',
+    vite: {
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, '..'),
+            },
         },
-        link: "https://aosc.io",
-      },
-    ],
-
-    // outline: false,
-    aside: false,
-    docFooter: {
-      prev: false,
-      next: false,
     },
-  },
-  locales: {
-    root: { label: "简体中文", ...zh },
-  },
-  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
-});
+    description: 'AOSC Mascots website',
+    themeConfig: {
+        socialLinks: [
+            {
+                icon: {
+                    svg: '<svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg"><g transform="translate(0,-50)"><circle cx="501.58" cy="552.36" r="475.19" fill="#000"/><circle cx="524" cy="550.36" r="54.07" fill="#fff"/><path d="M513.84 176.91A373.65 373.65 0 0 0 140.19 550.57 373.65 373.65 0 0 0 513.84 924.22v-182.5a191.87 191.87 0 0 1-181.71-191.36 191.87 191.87 0 0 1 181.71-191.51z" fill="#c00000"/><path d="M514.77 394.05a156.09 156.09 0 0 0-156.09 156.09 156.09 156.09 0 0 0 156.09 156.08v-74.27a82.15 82.15 0 0 1-72.92-81.59 82.15 82.15 0 0 1 72.92-81.53z" fill="#0070c0"/><path d="M531.57 230.53v160.87a159.34 159.34 0 0 1 151.77 158.96 159.34 159.34 0 0 1-151.77 159.07v160.87a319.89 319.89 0 0 0 319.88-319.9 319.89 319.89 0 0 0-319.88-319.87z" fill="#ffc000"/></g></svg>',
+                },
+                link: 'https://aosc.io',
+            },
+        ],
+
+        // outline: false,
+        aside: false,
+        docFooter: {
+            prev: false,
+            next: false,
+        },
+        search: {
+            provider: 'local',
+        },
+    },
+    locales: {
+        root: { label: '简体中文', ...zh },
+    },
+    head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+})
--- docs/.vitepress/locales/zh.js
+++ docs/.vitepress/locales/zh.js
@@ -1,84 +1,124 @@
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress'
 
 export const zh = defineConfig({
-  lang: "zh",
-  title: "安安和同同",
-  themeConfig: {
-    nav: [
-      { text: "首页", link: "/" },
-      { text: "关于安安", link: "/about-anan" },
-      { text: "关于同同", link: "/about-tongtong" },
-      { text: "相册", link: "/gallery" },
-      // { text: '故事', link: '/stories' },
-      { text: "文档中心", link: "/information/" },
-    ],
-
-    sidebar: {
-      "/information/": [
-        {
-          text: "文档中心",
-          link: "/information/",
-          items: [
-            {
-              text: "设定稿",
-              items: [
-                {
-                  text: "立绘",
-                  link: "/information/design/mascots",
+    lang: 'zh',
+    title: '安安和同同',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '关于安安', link: '/about-anan' },
+            { text: '关于同同', link: '/about-tongtong' },
+            { text: '相册', link: '/gallery' },
+            // { text: '故事', link: '/stories' },
+            { text: '文档中心', link: '/information/' },
+        ],
+        search: {
+            options: {
+                locales: {
+                    root: {
+                        translations: {
+                            button: {
+                                buttonText: '搜索',
+                                buttonAriaLabel: '搜索',
+                            },
+                            modal: {
+                                displayDetails: '显示详细列表',
+                                resetButtonTitle: '重置搜索',
+                                backButtonTitle: '关闭搜索',
+                                noResultsText: '没有结果',
+                                footer: {
+                                    selectText: '选择',
+                                    selectKeyAriaLabel: '输入',
+                                    navigateText: '导航',
+                                    navigateUpKeyAriaLabel: '上箭头',
+                                    navigateDownKeyAriaLabel: '下箭头',
+                                    closeText: '关闭',
+                                    closeKeyAriaLabel: 'Esc',
+                                },
+                            },
+                        },
+                    },
                 },
-                {
-                  text: "Minecraft 人物模型 (Figura)",
-                  link: "/information/design/figura",
-                },
-                {
-                  text: "贴图包",
-                  link: "/information/design/stickers",
-                },
-                {
-                  text: "Fumo 设计稿",
-                  link: "/information/design/fumo",
-                },
-                {
-                  text: "通行证周边设计稿",
-                  link: "/information/design/pass",
-                },
-              ],
             },
-            { text: "接收稿件", link: "/information/submit-arts" },
-            {
-              text: "导出作品",
-              items: [
-                {
-                  text: "CLIP STUDIO PAINT / 优动漫 PAINT",
-                  link: "/information/export/from-csp",
-                },
-                { text: "PaintTool SAI", link: "/information/export/from-sai" },
-                { text: "openCanvas", link: "/information/export/from-oc" },
-                {
-                  text: "FireAlpaca / MediBang Paint",
-                  link: "/information/export/from-mdp",
-                },
-                {
-                  text: "Corel Painter",
-                  link: "/information/export/from-corel",
-                },
-                {
-                  text: "Procreate",
-                  link: "/information/export/from-procreate",
-                },
-                { text: "画世界", link: "/information/export/from-hsj" },
-                { text: "天生会画", link: "/information/export/from-gopaint" },
-              ],
-            },
-          ],
         },
-      ],
-      "/stories/": [
-        {
-          text: "故事",
-          items: [],
+        sidebar: {
+            '/information/': [
+                {
+                    text: '文档中心',
+                    link: '/information/',
+                    items: [
+                        {
+                            text: '设定稿',
+                            items: [
+                                {
+                                    text: '立绘',
+                                    link: '/information/design/mascots',
+                                },
+                                {
+                                    text: 'Minecraft 人物模型 (Figura)',
+                                    link: '/information/design/figura',
+                                },
+                                {
+                                    text: '贴图包',
+                                    link: '/information/design/stickers',
+                                },
+                                {
+                                    text: 'Fumo 设计稿',
+                                    link: '/information/design/fumo',
+                                },
+                                {
+                                    text: '通行证周边设计稿',
+                                    link: '/information/design/pass',
+                                },
+                            ],
+                        },
+                        { text: '接收稿件', link: '/information/submit-arts' },
+                        {
+                            text: '导出作品',
+                            items: [
+                                {
+                                    text: 'CLIP STUDIO PAINT / 优动漫 PAINT',
+                                    link: '/information/export/from-csp',
+                                },
+                                {
+                                    text: 'PaintTool SAI',
+                                    link: '/information/export/from-sai',
+                                },
+                                {
+                                    text: 'openCanvas',
+                                    link: '/information/export/from-oc',
+                                },
+                                {
+                                    text: 'FireAlpaca / MediBang Paint',
+                                    link: '/information/export/from-mdp',
+                                },
+                                {
+                                    text: 'Corel Painter',
+                                    link: '/information/export/from-corel',
+                                },
+                                {
+                                    text: 'Procreate',
+                                    link: '/information/export/from-procreate',
+                                },
+                                {
+                                    text: '画世界',
+                                    link: '/information/export/from-hsj',
+                                },
+                                {
+                                    text: '天生会画',
+                                    link: '/information/export/from-gopaint',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+            '/stories/': [
+                {
+                    text: '故事',
+                    items: [],
+                },
+            ],
         },
-      ],
     },
-  },
-});
+})
